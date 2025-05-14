616cb19393c7aa88963f03083b13d4729dc7f420
I9ONA <marociona04@gmail.com>
2025-05-14 23:44:29 +0100

    semi-final

--- database/seeders/UtilisateurSeeder.php
+++ database/seeders/UtilisateurSeeder.php
@@ -13,7 +13,7 @@ class UtilisateurSeeder extends Seeder
         ['nom_user' => 'Ahmed', 'age' => 30, 'telephone' => '0612345678', 'email' => 'ahmed@dr.com', 'adresse' => 'Casablanca', 'password'=>'12345' ,'id_roles' => 1],
         ['nom_user' => 'Sara', 'age' => 28, 'telephone' => '0612345679', 'email' => 'sara@complexe.com', 'adresse' => 'Rabat','password'=>'12345', 'id_roles' => 2],
         ['nom_user' => 'Yassine', 'age' => 35, 'telephone' => '0612345680', 'email' => 'yassine@efp.com', 'adresse' => 'Marrakech','password'=>'12345', 'id_roles' => 3],
-        ['nom_user' => 'Khadija', 'age' => 32, 'telephone' => '0612345681', 'email' => 'khadija@formateur.com', 'adresse' => 'Tanger','password'=>'12345', 'id_roles' => 4],
+        ['nom_user' => 'Khadija', 'age' => 32, 'telephone' => '0612345681', 'email' => 'hiba@formateur.com', 'adresse' => 'Tanger','password'=>'12345', 'id_roles' => 4],
     ]);
 }
 }
