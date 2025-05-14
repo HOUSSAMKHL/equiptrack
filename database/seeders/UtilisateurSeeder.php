261a04c140d84fabb398a2ab8420d5c93994f0a3
I9ONA <marociona04@gmail.com>
2025-05-14 23:54:03 +0100

    semi-final

--- database/seeders/UtilisateurSeeder.php
+++ database/seeders/UtilisateurSeeder.php
@@ -10,10 +10,10 @@ class UtilisateurSeeder extends Seeder
     public function run(): void
 {
     DB::table('utilisateurs')->insert([
-        ['nom_user' => 'Ahmed', 'age' => 30, 'telephone' => '0612345678', 'email' => 'ahmed@dr.com', 'adresse' => 'Casablanca', 'password'=>'12345' ,'id_roles' => 1],
-        ['nom_user' => 'Sara', 'age' => 28, 'telephone' => '0612345679', 'email' => 'sara@complexe.com', 'adresse' => 'Rabat','password'=>'12345', 'id_roles' => 2],
-        ['nom_user' => 'Yassine', 'age' => 35, 'telephone' => '0612345680', 'email' => 'yassine@efp.com', 'adresse' => 'Marrakech','password'=>'12345', 'id_roles' => 3],
-        ['nom_user' => 'Khadija', 'age' => 32, 'telephone' => '0612345681', 'email' => 'hiba@formateur.com', 'adresse' => 'Tanger','password'=>'12345', 'id_roles' => 4],
+        ['nom_user' => 'oussama', 'age' => 22, 'telephone' => '0612345678', 'email' => 'oussamamame@efp.com', 'adresse' => 'Casablanca', 'password'=>'12345' ,'id_roles' => 1],
+        ['nom_user' => 'jalal', 'age' => 20, 'telephone' => '0612345679', 'email' => 'jalalmahmoudi@efp.com', 'adresse' => 'Rabat','password'=>'12345', 'id_roles' => 2],
+        ['nom_user' => 'houssam', 'age' => 22, 'telephone' => '0612345680', 'email' => 'houssamkhalil@efp.com', 'adresse' => 'Marrakech','password'=>'12345', 'id_roles' => 3],
+        ['nom_user' => 'omar', 'age' => 20, 'telephone' => '0612345681', 'email' => 'omar@efp.com', 'adresse' => 'Tanger','password'=>'12345', 'id_roles' => 4],
     ]);
 }
 }
