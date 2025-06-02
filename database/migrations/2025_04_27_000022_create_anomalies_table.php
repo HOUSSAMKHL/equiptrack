3f70b25ed17c93ad1aa1ea08294019415716c777
JalalMahmoudi <marcosgaming432@gmail.com>
2025-06-02 16:22:38 +0100

    sf20

--- database/migrations/2025_04_27_000022_create_anomalies_table.php
+++ database/migrations/2025_04_27_000022_create_anomalies_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->boolean('anomalie_resolue');
             $table->text('pieces_rechange')->nullable();
             $table->string('status')->default('open');
-            $table->string('priorite')->default('low');
+            $table->string('priorite')->default('faible');
             $table->foreignId('id_user')->constrained('utilisateurs')->onDelete('cascade');
             $table->foreignId('id_intervenant')->nullable()->constrained('intervenants')->onDelete('cascade');
             $table->foreignId('id_equipement')->constrained('equipement_tracables')->onDelete('cascade');
